1672ad78d6efa23429e817f630925865b2d2ab82
Ionut-Laurentiu Holbea <ilholbea@gmail.com>
2022-12-16 20:32:03 +0100

    Test API fetch with Axios in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ReactElement, useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+type ApiResponse = {
+  copyright: string;
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+};
+
+interface Response {
+  copyright: string;
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+}
+
+type GetApiResponse = {
+  data: ApiResponse[];
+};
+
+async function getImages() {
+  try {
+    const { data, status } = await axios.get<GetApiResponse>('https://go-apod.herokuapp.com/apod', {
+      headers: {
+        Accept: 'application/json'
+      }
+    });
+
+    console.log('response status: ', status);
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log('error message: ', error.message);
+      return error.message;
+    } else {
+      console.log('unexpected error: ', error);
+      return 'An unexpected error occurred';
+    }
+  }
+}
+
+function App(): ReactElement {
+  const [response, setResponse] = useState({
+    copyright: '',
+    date: '',
+    explanation: '',
+    hdurl: '',
+    media_type: '',
+    service_version: '',
+    title: '',
+    url: ''
+  });
+
+  useEffect(() => {
+    getImages().then((data) => {
+      setResponse(JSON.parse(JSON.stringify(data)) as Response);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{response.copyright}</p>
+      <p>{response.date}</p>
+      <p>{response.explanation}</p>
+      <p>{response.hdurl}</p>
+      <p>{response.media_type}</p>
+      <p>{response.service_version}</p>
+      <p>{response.title}</p>
+      <p>{response.url}</p>
     </div>
   );
 }
